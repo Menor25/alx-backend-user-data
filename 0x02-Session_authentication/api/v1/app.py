7474fee078a7fd160d573949d2e01d791c8cb4ca
Menor25 <ajirimenor@gmail.com>
2023-02-09 13:21:28 +0100

    All tasks done

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,83 +2,95 @@
 """
 Route module for the API
 """
-import os
+from os import getenv
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_db_auth import SessionDBAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS, cross_origin
+from flask_cors import (CORS, cross_origin)
+import os
+from typing import List, TypeVar
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+AUTH_TYPE = getenv('AUTH_TYPE')
 
-if os.getenv("AUTH_TYPE") == "auth":
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
     auth = Auth()
-
-if os.getenv("AUTH_TYPE") == "basic_auth":
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
-if os.getenv("AUTH_TYPE") == "session_auth":
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-
-if os.getenv("AUTH_TYPE") == "session_exp_auth":
+elif AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
-
-if os.getenv("AUTH_TYPE") == "session_db_auth":
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
-
-
-@app.before_request
-def before_request_callback():
-    """Before request"""
-    if auth:
-        excluded_paths = [
-            "/api/v1/status/",
-            "/api/v1/unauthorized/",
-            "/api/v1/forbidden/",
-            "/api/v1/auth_session/login/",
-        ]
-        res = auth.require_auth(request.path, excluded_paths)
-        if res:
-            if isinstance(auth, SessionAuth):
-                if auth.session_cookie(request) is None:
-                    abort(401)
-            else:
-                if auth.authorization_header(request) is None:
-                    abort(401)
-
-            current_user = auth.current_user(request)
-            if current_user is None:
-                abort(403)
-
-            request.current_user = current_user
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unauthorized handler"""
+    """Handle a unauthorized access
+        Args:
+            error: Error catch
+        Return:
+            Info of the error
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler"""
+    """Handle a forbidden resource
+        Args:
+            error: Error catch
+        Return:
+            Info of the error
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
+@app.before_request
+def before_request() -> str:
+    """Execute before each request
+        Return:
+            String or nothing
+    """
+    if auth is None:
+        return
+
+    expath = ['/api/v1/status/',
+              '/api/v1/unauthorized/',
+              '/api/v1/forbidden/',
+              '/api/v1/auth_session/login/']
+
+    if not (auth.require_auth(request.path, expath)):
+        return
+
+    if (auth.authorization_header(request)) is None\
+       and auth.session_cookie(request) is None:
+        abort(401)
+
+    current_user = auth.current_user(request)
+    if current_user is None:
+        abort(403)
+
+    request.current_user = current_user
+
+
 if __name__ == "__main__":
-    host = os.getenv("API_HOST", "0.0.0.0")
-    port = os.getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
